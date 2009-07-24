3fd6499b9e8e2bf7c453a3a31e3d6a46fdd3212c
DFischer <me@danielfischer.com>
2009-07-23 21:36:49 -0700

    Essentials to get up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => 'pages', :action => 'home'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
