2c01d921e6d083a718f19bdf0880ec8193dd5a1a
deadprogrammer <ron.evans@gmail.com>
2009-07-29 22:18:42 -0700

    Initial specs that pass

--- spec/models/presentation_link_spec.rb
+++ spec/models/presentation_link_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe PresentationLink do
   before(:each) do
     @valid_attributes = {
-      
+      :link => 'http://www.deadprogrammersociety.com'
     }
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe User do
   before(:each) do
     @valid_attributes = {
-      
+      :login => 'joe',
+      :password => '1234',
+      :password_confirmation => '1234',
+      :email => 'joe@hey.com'
     }
   end
 
