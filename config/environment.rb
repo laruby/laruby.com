4aebdc603318ad4f703f9f321f60a672a0e912a6
retr0h <john@dewey.ws>
2009-08-28 23:21:25 -0700

    Reworked how we include gems.

--- config/environment.rb
+++ config/environment.rb
@@ -1,59 +1,49 @@
-# Be sure to restart your server when you modify this file
+### Be sure to restart your server when you modify this file.
 
-# Specifies gem version of Rails to use when vendor/rails is not present
+##
+# Specifies gem version of Rails to use when vendor/rails is not present.
 RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
-# Bootstrap the Rails environment, frameworks, and default configuration
+##
+# Bootstrap the Rails environment, frameworks, and default configuration.
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  ##
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
 
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
+  ##
   # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
-	config.gem "rspec", :lib => false, :version => ">= 1.2.0" 
-	config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0" 
-	config.gem "haml", :version => ">= 2.2.2" 
-	config.gem "json", :lib => false, :version => "= 1.1.3" 
-	config.gem "binarylogic-authlogic", :lib => 'authlogic', :source => 'http://gems.github.com/'
-	config.gem "BRIMIL01-meetup_api", :lib => 'meetup_api', :source => 'http://gems.github.com/'
-	config.gem "twitter"
-	config.gem "mechanize"
-	config.gem "cucumber", :version => "~> 0.3.98"
-  # Only include action_mailer_tls for ruby 1.8.6
-	`/usr/bin/env ruby -v`.match(/^ruby (\d)\.(\d)\.(\d)/)
-	RUBY_VERSION_NUM = "#{$1}#{$2}#{$3}".to_i
-  if RUBY_VERSION_NUM < 187
-	  config.gem "openrain-action_mailer_tls", :lib => "smtp_tls.rb", :source => "http://gems.github.com"
+  config.gem "haml", :version => "2.2.3"
+  config.gem "BRIMIL01-meetup_api", :lib => 'meetup_api', :source => 'http://gems.github.com/'
+  config.gem "json", :version => ">=1.1.0"
+  config.gem "binarylogic-authlogic", :lib => 'authlogic', :source => 'http://gems.github.com/'
+  config.gem "twitter"
+  config.gem "mechanize"
+
+  ##
+  # Development gems.
+  if %(development test).include? RAILS_ENV
+    config.gem "webrat", :version => "0.5.3"
+    config.gem "rspec", :lib => false, :version => "1.2.8"
+    config.gem "rspec-rails", :lib => false, :version => "1.2.7.1"
+    config.gem "cucumber", :version => "0.3.98"
   end
 
-  # Only load the plugins named here, in the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+  if RUBY_VERSION == "1.8.7"
+    config.gem "openrain-action_mailer_tls", :lib => "smtp_tls", :source => "http://gems.github.com"
+  end
 
+  ##
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
+  ##
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
-
-  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :de
-  
-  #config.action_controller.session_store = :active_record_store
 end
 require 'bigdecimal'
--- config/initializers/smtp_gmail.rb
+++ config/initializers/smtp_gmail.rb
@@ -1,12 +1,8 @@
-# This file is automatically copied into RAILS_ROOT/initializers
+### This file is automatically copied into RAILS_ROOT/initializers/.
 
-if RUBY_VERSION_NUM < 187
-  require "smtp_tls"
-end
-
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.delivery_method        = :smtp
+ActionMailer::Base.perform_deliveries     = true
+ActionMailer::Base.raise_delivery_errors  = true
 
 begin
   LARUBY_EMAIL = Configuration.find_by_name("mail_user_name").value
