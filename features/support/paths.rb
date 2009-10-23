4dd97c5e9f6823dc934852ce1083d99a3168eac9
amikula <amikula@gmail.com>
2009-10-22 20:58:44 -0700

    Upgrading cucumber to 0.4.2

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -7,21 +7,11 @@ module NavigationHelpers
   #
   def path_to(page_name)
     case page_name
-    
-    when /the homepage/
-      '/'
-    when /the presentation index/
-      presentations_path
-    when /the login page/
-      login_path
-    when /the new presentation page/
-      new_presentation_path
-    # Add more mappings here.
-    # Here is a more fancy example:
-    #
-    #   when /^(.*)'s profile page$/i
-    #     user_profile_path(User.find_by_login($1))
 
+    when /the homepage/: '/'
+    when /the presentation index/: presentations_path
+    when /the login page/: login_path
+    when /the new presentation page/: new_presentation_path
     else
       raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
         "Now, go and add a mapping in #{__FILE__}"
