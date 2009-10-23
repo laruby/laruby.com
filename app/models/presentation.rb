a72ead82e6689597f0cea534467c6a8fe7e59260
amikula <amikula@gmail.com>
2009-10-22 21:41:44 -0700

    Fixing validation for subject so we only get one validation error

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -1,12 +1,10 @@
 class Presentation < ActiveRecord::Base
   belongs_to :submitter, :class_name => "User", :foreign_key => :submitted_by
-  has_many :links, :class_name => 'PresentationLink' 
-  
-  validates_presence_of :subject, :on => :create, :message => "can't be blank"
+  has_many :links, :class_name => 'PresentationLink'
+
   validates_length_of :subject, :in => 6..60
-  
+
   named_scope :proposed, :conditions => {:presentation_date => nil}
   named_scope :upcoming, :conditions => ['presentation_date >= ?', Date.today]
   named_scope :past, :conditions => ['presentation_date < ?', Date.today]
-  
 end
--- spec/models/presentation_spec.rb
+++ spec/models/presentation_spec.rb
@@ -10,31 +10,29 @@ describe Presentation do
   it "should create a new instance given valid attributes" do
     Presentation.create!(@valid_attributes)
   end
-  
+
   it "should require a subject" do
-	  @presentation = Presentation.new(@valid_attributes.except(:subject))
-		@presentation.should have(1).errors_on(:subject)
-	end
-	
-	describe "named scopes" do
-	
-	  before :each do
-	    @proposed = Presentation.create(@valid_attributes)
-	    @upcoming = Presentation.create(@valid_attributes.merge(:presentation_date => Date.tomorrow))
-	    @past = Presentation.create(@valid_attributes.merge(:presentation_date => Date.yesterday))
-    end
-	
-  	it "should find proposed presentations" do
-  	  Presentation.proposed.should == [@proposed]
+    @presentation = Presentation.new(@valid_attributes.except(:subject))
+    @presentation.should have(1).errors_on(:subject)
+  end
+
+  describe "named scopes" do
+    before :each do
+      @proposed = Presentation.create(@valid_attributes)
+      @upcoming = Presentation.create(@valid_attributes.merge(:presentation_date => Date.tomorrow))
+      @past = Presentation.create(@valid_attributes.merge(:presentation_date => Date.yesterday))
     end
 
-  	it "should find upcoming presentations" do
-  	  Presentation.upcoming.should == [@upcoming]	  
+    it "should find proposed presentations" do
+      Presentation.proposed.should == [@proposed]
     end
 
-  	it "should find past presentations" do
-  	  Presentation.past.should == [@past]
+    it "should find upcoming presentations" do
+      Presentation.upcoming.should == [@upcoming]
     end
 
+    it "should find past presentations" do
+      Presentation.past.should == [@past]
+    end
   end
 end
