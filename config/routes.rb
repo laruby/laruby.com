91ae605eeb907caba284274a2a56fcc588d56d5d
Giles Bowkett <gilesb@gmail.com>
2010-02-03 16:25:35 -0800

    comments removed and whitespace balanced

--- config/routes.rb
+++ config/routes.rb
@@ -4,59 +4,40 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :resources
   map.resources :presentations
   map.resource :account, :controller => "users" do |account|
-    account.link_meetup 'link', :action => 'link_meetup_account', :controller => "users"
-    account.resend_activation 'resend_activation', :action => 'resend_activation', :controller => "users"
+    account.link_meetup 'link',
+                        :action => 'link_meetup_account',
+                        :controller => "users"
+    account.resend_activation 'resend_activation',
+                              :action => 'resend_activation',
+                              :controller => "users"
   end
-  #map.resources :users
   map.resource :user_session
-  map.login 'login', :controller => "user_sessions", :action => "new"
-  map.logout 'logout', :controller => "user_sessions", :action => "destroy"
-  map.activate 'activate/:token', :controller => "users", :action => "activate", :token => nil
+  map.login 'login',
+            :controller => "user_sessions",
+            :action => "new"
+  map.logout 'logout',
+             :controller => "user_sessions",
+             :action => "destroy"
+  map.activate 'activate/:token',
+               :controller => "users",
+               :action => "activate",
+               :token => nil
 
-  map.root :controller => 'pages', :action => 'home'
-  map.map '/map/:event_id', :controller => 'pages', :action => 'map', :event_id => nil
-  map.events '/events/:past', :controller => 'pages', :action => 'events', :past => nil
-  map.static_page ':page', :controller => 'pages', :action => 'show', :page => /about|contact|newbies|community|resources|connect/
+  map.root :controller => 'pages',
+           :action => 'home'
+  map.map '/map/:event_id',
+          :controller => 'pages',
+          :action => 'map',
+          :event_id => nil
+  map.events '/events/:past',
+             :controller => 'pages',
+             :action => 'events',
+             :past => nil
+  map.static_page ':page',
+                  :controller => 'pages',
+                  :action => 'show',
+                  :page => /about|contact|newbies|community|resources|connect/
 
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
