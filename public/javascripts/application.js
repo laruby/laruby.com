2feb45d1059c0cbf8d7b20226c910fb6ebb79b5c
deadprogrammer <ron.evans@gmail.com>
2012-01-05 19:13:11 -0800

    Cleanup references and remove .gems file

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
       $("#LARubyfeed").tweet({
         avatar_size: 50,
         count: 10,
-        // username: ["laruby"],
         loading_text: "searching twitter...",
         query: "laruby",
         refresh_interval: 1000
