9a8643a203588332f912587169e8aa2ee8ae5c65
DFischer <me@danielfischer.com>
2012-11-29 17:41:52 -0800

    Put videos in

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,4 +10,15 @@ $(document).ready(function() {
       });
     });
 
+    $('#youtube_channel').youtubeChannel({
+      // username -or- query: required, username takes precedence over query
+      username: 'losangelesruby',
+      // startIndex: optional, must be >=1
+      startIndex: 1,
+      // maxResults: optional, defaults to 50
+      maxResults: 3,
+      // orderBy: optional, defaults to 'published'
+      orderBy: 'published'
+    });
+
 });
