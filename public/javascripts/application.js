0a8662ba2a2d81db9a7bd537a70646e74ac178db
DFischer <me@danielfischer.com>
2013-06-28 16:02:25 -0700

    Remove twitter library due to Twitter API changes. Fixed some really weird CSS. Could use some more tweaks (get rid of the %'s')

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,24 +1,14 @@
 $(document).ready(function() {
 
-  jQuery(function($){
-      $("#LARubyfeed").tweet({
-        avatar_size: 50,
-        count: 10,
-        loading_text: "searching twitter...",
-        query: "#laruby OR @laruby",
-        refresh_interval: 1000
-      });
-    });
-
-    $('#youtube_channel').youtubeChannel({
-      // username -or- query: required, username takes precedence over query
-      username: 'losangelesruby',
-      // startIndex: optional, must be >=1
-      startIndex: 1,
-      // maxResults: optional, defaults to 50
-      maxResults: 3,
-      // orderBy: optional, defaults to 'published'
-      orderBy: 'published'
-    });
+  $('#youtube_channel').youtubeChannel({
+    // username -or- query: required, username takes precedence over query
+    username: 'losangelesruby',
+    // startIndex: optional, must be >=1
+    startIndex: 1,
+    // maxResults: optional, defaults to 50
+    maxResults: 3,
+    // orderBy: optional, defaults to 'published'
+    orderBy: 'published'
+  });
 
 });
